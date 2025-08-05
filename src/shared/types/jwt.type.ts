98a264ff41a992d116bcacf80db98345ea759f5e
JMANPACHON27 <JMANPACHON27@outlook.com>
2025-08-05 19:07:30 +0700

    update contanst

--- src/shared/types/jwt.type.ts
+++ src/shared/types/jwt.type.ts
@@ -3,3 +3,10 @@ export interface TokenPayload {
   exp: number
   iat: number
 }
+
+export interface RefreshTokenPayload {
+  userId: number
+  exp: number
+  iat: number
+  jti: string // JWT ID for refresh token
+}
